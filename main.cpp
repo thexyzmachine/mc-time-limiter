17a6262804d19a339d91c31e2a7498c552873059
XYZ Machine <xyzmachine.fr@gmail.com>
2026-05-10 18:53:44 -0500

    Refactor Minecraft process handling functions

--- main.cpp
+++ main.cpp
@@ -1,71 +1,54 @@
 #include <windows.h>
-#include <tlhelp32.h>
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <ctime>
 
-bool isMinecraftOpen() {
-    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-
-    PROCESSENTRY32 process;
-    process.dwSize = sizeof(PROCESSENTRY32);
-
-    if (Process32First(snapshot, &process)) {
-        do {
-            if (_wcsicmp(process.szExeFile, L"javaw.exe") == 0) {
-                CloseHandle(snapshot);
-                return true;
-            }
-
-        } while (Process32Next(snapshot, &process));
-    }
-
-    CloseHandle(snapshot);
-    return false;
+bool checkIfMinecraftOpen() {
+    HWND hwnd = FindWindow(NULL, L"Minecraft");
+    return hwnd != NULL;
 }
 
-void closeMinecraft() {
-    HANDLE snapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
-
-    PROCESSENTRY32 process;
-    process.dwSize = sizeof(PROCESSENTRY32);
-
-    if (Process32First(snapshot, &process)) {
-        do {
-            if (_wcsicmp(process.szExeFile, L"javaw.exe") == 0) {
-
-                HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, process.th32ProcessID);
-
-                if (hProcess) {
-                    TerminateProcess(hProcess, 0);
-                    CloseHandle(hProcess);
-
-                    std::cout << "Minecraft closed.\n";
-                }
-            }
-
-        } while (Process32Next(snapshot, &process));
+void closeMc() {
+    HWND hwnd = FindWindow(NULL, L"Minecraft");
+    if (hwnd != NULL) {
+        PostMessage(hwnd, WM_CLOSE, 0, 0);
+        std::cout << "Minecraft closed.\n";
     }
-
-    CloseHandle(snapshot);
 }
 
 int main() {
-
+    int usedDay = -1;
+    bool timerStarted = false;
+    
     while (true) {
-
         std::this_thread::sleep_for(std::chrono::seconds(5));
-
+        std::time_t now = std::time(nullptr);
+        std::tm* localTime = std::localtime(&now);
+        int currentDay = localTime->tm_mday;
+        
+        if (currentDay != usedDay) {
+            timerStarted = false;
+        }
+        
         if (isMinecraftOpen()) {
-
-            std::cout << "Minecraft detected.\n";
-            std::cout << "Waiting 1 hour...\n";
-
-            std::this_thread::sleep_for(std::chrono::hours(1));
-
-            // Check again before closing
-            if (isMinecraftOpen()) {
-                closeMinecraft();
+            if (currentDay == usedDay) {
+                std::cout << "Minecraft already used today.\n";
+                closeMc();
+                continue;
+            }
+            if (!timerStarted) {
+                timerStarted = true;
+                std::cout << "Minecraft detected.\n";
+                std::cout << "Starting 1 hour timer...\n";
+                std::this_thread::sleep_for(std::chrono::hours(1));
+                if (checkIfMinecraftOpen()()) {
+                    closeMc();
+                    usedDay = currentDay;
+                    std::cout << "Time used for day "
+                              << usedDay
+                              << ".\n";
+                }
             }
         }
     }
