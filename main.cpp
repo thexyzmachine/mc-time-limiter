19fe2dd3241f1bd2e1b5d04e47835abe610bfeaf
XYZ Machine <xyzmachine.fr@gmail.com>
2026-05-10 19:04:08 -0500

    Refactor Minecraft management functions and file handling

--- main.cpp
+++ main.cpp
@@ -3,51 +3,111 @@
 #include <thread>
 #include <chrono>
 #include <ctime>
+#include <fstream>
+#include <string>
 
-bool checkIfMinecraftOpen() {
-    HWND hwnd = FindWindow(NULL, L"Minecraft");
-    return hwnd != NULL;
+const std::string FILE_NAME = "mc_data.txt";
+
+int loadUsedDay() {
+    std::ifstream file(FILE_NAME);
+    int day;
+
+    if (file >> day) {
+        return day;
+    }
+
+    return -1; 
+}
+
+void saveUsedDay(int day) {
+    std::ofstream file(FILE_NAME);
+    file << day;
+}
+
+bool isMinecraftOpen() {
+    bool found = false;
+
+    EnumWindows([](HWND hwnd, LPARAM lParam) -> BOOL {
+
+        wchar_t title[256];
+        GetWindowTextW(hwnd, title, 256);
+
+        std::wstring windowTitle = title;
+
+        if (windowTitle.find(L"Minecraft") != std::wstring::npos) {
+            *(bool*)lParam = true;
+            return FALSE;
+        }
+
+        return TRUE;
+
+    }, (LPARAM)&found);
+
+    return found;
 }
 
 void closeMc() {
-    HWND hwnd = FindWindow(NULL, L"Minecraft");
-    if (hwnd != NULL) {
-        PostMessage(hwnd, WM_CLOSE, 0, 0);
-        std::cout << "Minecraft closed.\n";
-    }
+    EnumWindows([](HWND hwnd, LPARAM) -> BOOL {
+
+        wchar_t title[256];
+        GetWindowTextW(hwnd, title, 256);
+
+        std::wstring windowTitle = title;
+
+        if (windowTitle.find(L"Minecraft") != std::wstring::npos) {
+            PostMessage(hwnd, WM_CLOSE, 0, 0);
+            std::cout << "Minecraft closed.\n";
+            return FALSE;
+        }
+
+        return TRUE;
+
+    }, 0);
 }
 
 int main() {
-    int usedDay = -1;
+
+    int usedDay = loadUsedDay();
     bool timerStarted = false;
-    
+
+    std::cout << "Loaded usedDay = " << usedDay << "\n";
+
     while (true) {
+
         std::this_thread::sleep_for(std::chrono::seconds(5));
+
         std::time_t now = std::time(nullptr);
-        std::tm* localTime = std::localtime(&now);
-        int currentDay = localTime->tm_mday;
-        
+        std::tm* local = std::localtime(&now);
+        int currentDay = local->tm_mday;
+
         if (currentDay != usedDay) {
             timerStarted = false;
         }
-        
+
         if (isMinecraftOpen()) {
-            if (currentDay == usedDay) {
-                std::cout << "Minecraft already used today.\n";
+
+            if (usedDay == currentDay) {
+                std::cout << "Limit already used today.\n";
                 closeMc();
                 continue;
             }
+
             if (!timerStarted) {
+
                 timerStarted = true;
-                std::cout << "Minecraft detected.\n";
-                std::cout << "Starting 1 hour timer...\n";
+
+                std::cout << "Minecraft detected. Starting 1 hour timer...\n";
+
                 std::this_thread::sleep_for(std::chrono::hours(1));
-                if (checkIfMinecraftOpen()()) {
+
+                if (isMinecraftOpen()) {
+
                     closeMc();
+
                     usedDay = currentDay;
-                    std::cout << "Time used for day "
-                              << usedDay
-                              << ".\n";
+                    saveUsedDay(usedDay);
+
+                    std::cout << "Saved usedDay = " << usedDay << "\n";
                 }
             }
         }
